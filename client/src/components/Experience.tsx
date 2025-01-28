e6a539a4bc14f94dab1c202454392c886dd5d4fd
Fatih Yonucuoglu <fatih.yonucuoglu@gmail.com>
2025-01-28 21:36:43 +0300

    guitar

--- client/src/components/Experience.tsx
+++ client/src/components/Experience.tsx
@@ -1,23 +1,37 @@
 import { Canvas } from "@react-three/fiber";
 import Drum from "./Drum";
 import { Environment, OrbitControls } from "@react-three/drei";
+import Guitar from "./Guitar";
+import { useControls } from "leva";
 
 const Experience = () => {
+  const environments = {
+    "Studio Small": "./studio_small_08_1k.hdr",
+    "Vignaioli Night": "./vignaioli_night_1k.hdr",
+  } as const;
+
+  type EnvironmentKey = keyof typeof environments;
+
+  const { environment }: { environment: EnvironmentKey } = useControls({
+    environment: { options: Object.keys(environments) as EnvironmentKey[] },
+  });
+
   return (
     <div className="h-dvh w-dvw">
       <Canvas camera={{ position: [0, 3, 5] }}>
         <color attach="background" args={["#0f0f0f"]} />
-        <directionalLight intensity={1} position={[0,5,0]} />
+        <directionalLight intensity={1} position={[0, 5, 0]} />
         <Environment
           environmentIntensity={1}
-          files={"./vignaioli_night_1k.hdr"}
+          files={environments[environment]}
         />
         <OrbitControls />
-        <Drum
+        {/* <Drum
           position={[-4, 0, 0]}
           rotation={[0, -Math.PI, 0]}
           scale={0.0025}
-        />
+        /> */}
+        <Guitar position={[0, 0, 0]} scale={10} />
       </Canvas>
     </div>
   );
