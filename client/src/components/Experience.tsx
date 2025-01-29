dc9c0fd398abb18ed8a30e4f3b42dce2b36e5901
Fatih Yonucuoglu <fatih.yonucuoglu@gmail.com>
2025-01-30 01:02:40 +0300

    key press

--- client/src/components/Experience.tsx
+++ client/src/components/Experience.tsx
@@ -1,8 +1,8 @@
 import { Canvas } from "@react-three/fiber";
 import Drum from "./Drum";
 import { Environment, OrbitControls } from "@react-three/drei";
-import Guitar from "./Guitar";
 import { useControls } from "leva";
+import Guitar from "./Guitar";
 
 const Experience = () => {
   const environments = {
@@ -18,7 +18,7 @@ const Experience = () => {
 
   return (
     <div className="h-dvh w-dvw">
-      <Canvas camera={{ position: [0, 3, 5] }}>
+      <Canvas camera={{ position: [0, 0, 5] }}>
         <color attach="background" args={["#0f0f0f"]} />
         <directionalLight intensity={1} position={[0, 5, 0]} />
         <Environment
@@ -31,7 +31,8 @@ const Experience = () => {
           rotation={[0, -Math.PI, 0]}
           scale={0.0025}
         /> */}
-        <Guitar position={[0, 0, 0]} scale={10} />
+        {/* <Guitar position={[0, 0, 0]} scale={10} /> */}
+        <Guitar position={[0, 0, 0]} scale={.05} />
       </Canvas>
     </div>
   );
